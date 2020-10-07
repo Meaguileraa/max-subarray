82cf20cea599270f3a7755a8c458893ebde258a1
Marissa Aguilera <marissaguilera@yahoo.com>
2020-10-06 20:40:57 -0700

    Problem completed

--- problem.py
+++ problem.py
@@ -11,14 +11,37 @@
 def max_subarray(nums):
     """Given an integer array nums, find the contiguous subarray ."""
 
+    current_sum = 0
     largest_sum = 0 
+    all_negatives = True 
 
+
+
+    # if len(nums) > 0 and len(nums) < 2: 
+    #     largest_sum = nums[0]
+    #     current_sum = nums[0]
 
     for num in nums:
-        
-        
-    # if len(nums) == 1:
-    #     return 
+
+        if num > 0: 
+            all_negatives = False 
+
+
+        if current_sum + num < 0:
+            current_sum = 0 
+        else:
+            current_sum += num
+            #if current sum is largest 
+
+        if current_sum > largest_sum:
+            largest_sum = current_sum
+
+
+    if all_negatives:
+        largest_sum = max(nums)  
+    
+
+    return largest_sum
 
 
 
@@ -28,3 +51,9 @@ print(max_subarray([1])) #1
 print(max_subarray([-1])) #-1
 print(max_subarray([0])) #0
 print(max_subarray([-2,1,-3,4,-1,2,1,-5,4])) #6,  [4,-1,2,1]
+print(max_subarray([8,9,-1,0])) #17
+
+
+#subarray is anything from one number to the whole array 
+#go from each end and check the highest sum 
+# if one sum is bigger record that as the largest sum 
